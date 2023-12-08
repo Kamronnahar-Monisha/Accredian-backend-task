dee82616c8159e54485d393ed1f502da6130df1b
Kamronnahar-Monisha <kamronnaharmonisha12@gmail.com>
2023-12-08 17:19:31 +0600

    mysql is connected to backend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
 require('dotenv').config();
 
 
@@ -7,9 +8,47 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+
 //middle wires added
 app.use(cors());
 app.use(express.json());
+
+
+
+//creating a connection
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: 'accredian'
+});
+
+
+// Connect to MySQL
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+
+// Close the connection when done
+connection.end((err) => {
+    if (err) {
+        console.error('Error closing database connection:', err);
+        return;
+    }
+    console.log('Disconnected from MySQL database');
+});
+
+
+app.post('/user', async (req, res) => {
+    const user = req.body;
+    console.log(user);
+});
+
 
 
 
